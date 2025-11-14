86d330b292f702000a24f44d1b0fda30dc7272cb
KotaVasavi <vasavikota10@gmail.com>
2025-11-14 17:38:14 +0530

    changes related to deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const API = import.meta.env.VITE_API_URL;
+
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api', // Your backend API URL
+  baseURL: `${API}/api`,
   headers: {
     'Content-Type': 'application/json',
   },
